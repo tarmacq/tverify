bff8fc4a46e757c3505c1307cb9bcbd757584ae3
tarmacq studios <jsalifstgat@gmail.com>
2025-10-03 11:35:46 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,11 @@
-
-const SUPABASE_URL = 'https://lxtaitbzpvdzyonxhfmm.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4dGFpdGJ6cHZkenlvbnhoZm1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNTA3ODIsImV4cCI6MjA3NDkyNjc4Mn0.gNIq2oJFbKgz7TayICLi6djNB_j2PxAlAZYyE0s5amM';
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// supabase.js
+const supabase = window.supabase.createClient(
+  'https://lxtaitbzpvdzyonxhfmm.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4dGFpdGJ6cHZkenlvbnhoZm1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNTA3ODIsImV4cCI6MjA3NDkyNjc4Mn0.gNIq2oJFbKgz7TayICLi6djNB_j2PxAlAZYyE0s5amM',
+  {
+    auth: {
+      flowType: 'pkce', // enables secure redirect flow
+      detectSessionInUrl: true // auto-handles callback
+    }
+  }
+);
