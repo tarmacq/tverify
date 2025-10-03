ba4c4b9202e0c590fd543bbda14f4a1d633d3c85
tarmacq studios <jsalifstgat@gmail.com>
2025-10-03 10:54:57 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,4 @@
 
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://lxtaitbzpvdzyonxhfmm.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4dGFpdGJ6cHZkenlvbnhoZm1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNTA3ODIsImV4cCI6MjA3NDkyNjc4Mn0.gNIq2oJFbKgz7TayICLi6djNB_j2PxAlAZYyE0s5amM'
-const supabase = createClient(supabaseUrl, supabaseKey)
+const SUPABASE_URL = 'https://lxtaitbzpvdzyonxhfmm.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4dGFpdGJ6cHZkenlvbnhoZm1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNTA3ODIsImV4cCI6MjA3NDkyNjc4Mn0.gNIq2oJFbKgz7TayICLi6djNB_j2PxAlAZYyE0s5amM';
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
